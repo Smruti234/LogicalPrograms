704099ab9c8b204838378bc9849fee48feb52c9a
Smruti <lenkasmruti123@gmail.com>
2021-10-05 07:31:41 +0530

    Added: PrimeNumber

---
+++ PrimeNumber.java
@@ -0,0 +1,27 @@
+package LogicalPrograms;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	static void primeNum(int n) {
+		int flag = 0, i;
+		for (i = 2; i <= (n / 2); i++) {
+			if ((n % i) == 0)
+				flag = 1;
+		}
+		if (n == 1)
+			System.out.println("1 is neither prime nor composite");
+		else if (flag == 0)
+			System.out.println(n + " is a prime Number");
+		else
+			System.out.println(n + " is not a prime Number");
+	}
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a Number");
+		int number = s.nextInt();
+		primeNum(number);
+	}
+}
+
